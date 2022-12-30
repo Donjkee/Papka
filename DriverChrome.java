29a6ecfb3fb90afb1962181785b2697412416fd5
Donjkee <121558360+Donjkee@users.noreply.github.com>
2022-12-30 14:24:35 +0200

    Update DriverChrome.java

--- DriverChrome.java
+++ DriverChrome.java
@@ -1,14 +1,11 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.jetbrains.annotations.NotNull;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.Map;
 
 public class DriverChrome
 {
         private WebDriver driver;
+        private int factHeight = 0;
 
         public DriverChrome(String url) throws InterruptedException
         {
@@ -27,47 +24,61 @@ public class DriverChrome
             this.driver = driver;
         }
 
-        public boolean check(Map<String, String> map)
+        public int checkTable()
         {
-            WebElement element;
-            String text;
-            for (String key : map.keySet())
-            {
-                if(key.equals("Country"))
-                {
-                    try
-                    {
-                        text = map.get(key);
-                        element = driver.findElement(By.xpath("//td/strong[text()='" + text + "']"));
-                    }
-                    catch (NoSuchElementException e)
-                    {
-                        return false;
-                    }
-                }
-                else if(key.equals("Capitals"))
-                {
-                    try
-                    {
-                        text = map.get(key);
-                        element = driver.findElement(By.xpath("//td[text()='"+
-                                text +"']/preceding-sibling::td[2]"));
-                    }
-                    catch (NoSuchElementException e)
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    return false;
-                }
+            WebElement table = driver.findElement(By.xpath("//table"));
+            int rowCount = Integer.parseInt(table.getAttribute("aria-rowcount"));
+            int colCount = Integer.parseInt(table.getAttribute("aria-colcount"));
 
+            WebElement scrollBar = driver.findElement(By.xpath("(//smart-scroll-bar)[1]"));
+            WebElement scrollDown = driver.findElement(By.xpath("//div[@class='smart-scroll-button smart-arrow-down']"));
+            int scrollPerClick = Integer.parseInt(scrollBar.getAttribute("thumbSize"));
+            int scrollHeight = Integer.parseInt(driver.findElement(By.xpath("//tbody/tr[@aria-rowindex='1']"))
+                    .getAttribute("scrollHeight"));
+
+            WebElement[] elements = new WebElement[colCount];
+
+            for(int i = 0; i < colCount; i++)
+            {
+                elements[i] = driver.findElement(By.xpath("(//tr/th)[" + (i + 1) + "]"));
             }
 
-            return true;
+            WebElement rowElement, colElement;
+            for(int i = 1; i <= rowCount; i++)
+            {
+                rowElement = driver.findElement(By.xpath("//tbody/tr[@aria-rowindex=" + i + "]"));
+                for(int j = 0; j < colCount; j++)
+                {
+                    try
+                    {
+                        colElement = rowElement.findElement(By.xpath("(//td)["+ (j + 1) +"]"));
+                    }
+                    catch (NoSuchElementException e)
+                    {
+                        return i;
+                    }
+
+                    if(!colElement.getAttribute("ariaColIndex").equals(elements[j].getAttribute("ariaColIndex")))
+                    {
+                        return i;
+                    }
+                }
+                scroll(scrollBar, scrollDown, scrollPerClick, scrollHeight);
+                System.out.println(i);
+            }
+            return 0;
         }
 
+        void scroll(@NotNull WebElement scrollBar, WebElement scrollButton, int scrollPerClick, int px)
+        {
+            int i = Integer.parseInt(scrollBar.getAttribute("aria-valuenow"));
+            int end = factHeight + px;
+            for(; i < end; i += scrollPerClick)
+            {
+                scrollButton.click();
+            }
+            factHeight += px;
+        }
 
         public void quit()
         {
