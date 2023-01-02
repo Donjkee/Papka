2ae4cbbe86590f8e7787ae44fceebc29c64f8bba
Donjkee <121558360+Donjkee@users.noreply.github.com>
2023-01-02 13:00:37 +0200

    Create Main.java

---
+++ table/Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    public static void main(String []args) throws InterruptedException
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\tools\\chromedrivers\\chromedriver.exe");
+        DriverChrome test = new DriverChrome("https://www.htmlelements.com/demos/table/overview/index.htm");
+
+        int number = test.checkTable();
+        if(number != 0)
+        {
+            System.out.println("Wrong table number: " + number + "!");
+        }
+        else
+        {
+            System.out.println("Everything is ok!");
+        }
+
+        test.quit();
+    }
+}
