8fbf75efa7f5b438b330d7c31d23decca1786253
Donjkee <121558360+Donjkee@users.noreply.github.com>
2023-01-03 14:53:57 +0200

    Update DriverChrome.java

--- table/DriverChrome.java
+++ table/DriverChrome.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,21 +27,24 @@ public class DriverChrome
             this.driver = driver;
         }
 
-        public int checkTable()
+        public void checkTable()
         {
             WebElement grid = driver.findElement(By.xpath("//table"));
-
             Table table = new Table(grid);
 
             table.setScrollBar(driver.findElement(By.xpath("(//smart-scroll-bar)[1]")));
-            table.setScrollButton(driver.findElement(By.xpath("//div[@class='smart-scroll-button smart-arrow-down']")));
-
+            table.setScrollDownButton(driver.findElement(By.xpath("//div[@class='smart-scroll-button smart-arrow-down']")));
+            table.setScrollUpButton(driver.findElement(By.xpath("//div[@class='smart-scroll-button smart-arrow-up']")));
             int scrollHeight = Integer.parseInt(driver.findElement(By.xpath("//table/thead"))
                     .getAttribute("clientHeight"));
-
             table.setScrollHeight(scrollHeight);
 
-            return table.checkTable();
+            Actions actions = new Actions(driver);
+            table.setActions(actions);
+
+            Map<String, String> map = new HashMap<>();
+            map.put("Product Name", "Ultrean 6 Quart Air Fryer");
+            table.verifyRow(map);
         }
 
         public void quit()
